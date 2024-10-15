c89e7f6752429062a3612aa7234b55f2b2c95988
lagudu-glitch <laguduvishnu@gmail.com>
2024-10-15 15:10:59 -0400

    Add word parse infrastructure

--- inc/parse.h
+++ inc/parse.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_LINE_LENGTH 1024
+#define MAX_WORDS 100       // Max number of words in a line
+#define MAX_CHARS 1024      // Max number of characters in a line
 
 // functions
 void _parse (char* fname);
--- src/parse.c
+++ src/parse.c
@@ -5,8 +5,12 @@ void
 _parse (char* fname)
 {
   // vars
-  int lc = 0;
-  char buff[MAX_LINE_LENGTH];
+  int  lc, wc;
+  char buff  [MAX_CHARS];
+  char *line [MAX_WORDS];
+  
+  lc = 0;
+  wc = 0;
 
   // open the file
   FILE *fp  = fopen (fname, "r");
@@ -25,10 +29,31 @@ _parse (char* fname)
   {
     lc++;
   }
-  
+
+  // read all the lines
+  while (fgets(buff, sizeof(buff), fp))
+  {
+    // Tokenize and process line
+    char *token = strtok(buff, " \t\n");
+    while (token != NULL && wc < MAX_WORDS)
+    {
+      line [wc] = malloc (strlen (token) + 1);
+      strcpy (line [wc], token);
+      wc++;
+      token = strtok (NULL, " \t\n");
+    }
+
+    printf ("Words in the line : \n");
+    for (int i = 0; i < wc; i++)
+    {
+      printf ("%s\n", line[i]);
+      free (line[i]);
+    }
+  }
+
   // print the number of lines
   printf ("Number of lines: %d\n", lc);
 
-  // close the file
+  // close the filegt
   fclose(fp);
 }
